417fc86934e5e3161b226af75f0bf3038a24d81f
Natasha <shotaoouji@gmail.com>
2025-06-16 10:29:08 -0300

    fix home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.scss";
 function Home() {
   return (
     <div className="main--inner">
-      <p className="intro">Hola, soy Natasha y no soy nati</p>
+      <p className="intro">Hola, soy Natasha y no soy Nati</p>
     </div>
   );
 }
